1c4479586dd0a9ba7831f17823a9acc317002e9e
FlorSing <fpsingcak@gmail.com>
2023-02-02 12:02:54 +0000

    gather current weather data

--- assets/script.js
+++ assets/script.js
@@ -31,9 +31,12 @@ $.ajax({
   localStorage.setItem('city',geoLocArray[0].name);
   localStorage.setItem('country',geoLocArray[0].country);
 });
+var city = localStorage.getItem('city');
+var country = localStorage.getItem('country');
+var lat = localStorage.getItem('lat');
+var lon = localStorage.getItem('lon');
 
 //url query output for use on dashboard = city, temp, wind, humidity
-
 var forecastQueryURL = "http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+APIKey;
 $.ajax({
   url:forecastQueryURL,
@@ -41,23 +44,22 @@ $.ajax({
 }).then(function(weather) {
   console.log(weather);
   var currWeather = weather
-  localStorage.setItem('date',currWeather[3].lat);
-  localStorage.setItem('lon',geoLocArray[0].lon);
-  localStorage.setItem('city',geoLocArray[0].name);
-  localStorage.setItem('country',geoLocArray[0].country);
+  localStorage.setItem('date',currWeather.list[0].dt);
+  localStorage.setItem('temp',currWeather.list[0].main.temp);
+  localStorage.setItem('wind',currWeather.list[0].wind.speed);
+  localStorage.setItem('humidity',currWeather.list[0].main.humidity);
 });
 
-var city = localStorage.getItem('city');
-var country = localStorage.getItem('country');
-var lat = localStorage.getItem('lat');
-var lon = localStorage.getItem('lon');
+var date = localStorage.getItem('date');
 var temp = localStorage.getItem('temp');
 var wind = localStorage.getItem('wind');
 var humidity = localStorage.getItem('humidity');
+
 console.log(city);
 console.log(country);
 console.log(lat);
 console.log(lon);
+console.log(moment.unix(date).format('DD MMM YYYY'));
 console.log(temp);
 console.log(wind);
 console.log(humidity);
