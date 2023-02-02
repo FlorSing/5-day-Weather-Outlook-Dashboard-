dc254d1c17ea87ab7c346c762d9ad7f2ca3494ac
FlorSing <fpsingcak@gmail.com>
2023-02-02 11:41:20 +0000

    updated js

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,20 @@
-// Add your own API key between the ""
+function renderButtons() {
+   var a = $("<button>").addClass("cities").text(readText);
+    $("#history").append(a);};
+
 var APIKey = "72380dfb30d88ca9844eebc1d2827077";
-
-// Here we are building the URL we need to query the database
-// var queryManilaURL = "https://api.openweathermap.org/data/2.5/weather?q=Manila&appid=" + APIKey;
-// console.log(queryManilaURL);
-// $.ajax({
-//   url: queryURL,
-//   method: "GET"
-// }).then(function(manilaQuery) {
-//   console.log(manilaQuery);
-// });
-//query using geographical coordinates
-//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-//query using geocoding via openweather api
-//http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-
-
-//console.log(searchInput);
 
 //determine what city is being searched
 function getSearchTextValue(){
   var searchInput = $("#search-input").val();
   localStorage.setItem('searchText', searchInput);}
+
 $("#search-button").on('click', function(){getSearchTextValue()});
 readText = localStorage.getItem('searchText');
 console.log(readText);
 $("#search-input").val(readText);
+
+renderButtons();
 
 //use the searched item in api url query
 var cityName = readText;
@@ -36,20 +25,28 @@ $.ajax({
   url: getGeoLocURL,
   method: "GET"
 }).then(function(geoLoc) {
-//  console.log(geoLoc);
   var geoLocArray = geoLoc
-  // console.log(geoLocArray[0].lat);
   localStorage.setItem('lat',geoLocArray[0].lat);
-  // console.log(geoLocArray[0].lon);
   localStorage.setItem('lon',geoLocArray[0].lon);
-  // console.log(geoLocArray[0].name);
   localStorage.setItem('city',geoLocArray[0].name);
-  // console.log(geoLocArray[0].country);
   localStorage.setItem('country',geoLocArray[0].country);
-  return geoLocArray;
 });
 
 //url query output for use on dashboard = city, temp, wind, humidity
+
+var forecastQueryURL = "http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+APIKey;
+$.ajax({
+  url:forecastQueryURL,
+  method: "GET"
+}).then(function(weather) {
+  console.log(weather);
+  var currWeather = weather
+  localStorage.setItem('date',currWeather[3].lat);
+  localStorage.setItem('lon',geoLocArray[0].lon);
+  localStorage.setItem('city',geoLocArray[0].name);
+  localStorage.setItem('country',geoLocArray[0].country);
+});
+
 var city = localStorage.getItem('city');
 var country = localStorage.getItem('country');
 var lat = localStorage.getItem('lat');
@@ -57,19 +54,11 @@ var lon = localStorage.getItem('lon');
 var temp = localStorage.getItem('temp');
 var wind = localStorage.getItem('wind');
 var humidity = localStorage.getItem('humidity');
-// console.log(city);
-// console.log(country);
-// console.log(lat);
-// console.log(lon);
-
-
-var forecastQueryURL = "http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+APIKey;
-$.ajax({
-  url:forecastQueryURL,
-  method: "GET"
-}).then(function(response) {
-
-  console.log(response);
-
-});
+console.log(city);
+console.log(country);
+console.log(lat);
+console.log(lon);
+console.log(temp);
+console.log(wind);
+console.log(humidity);
 
